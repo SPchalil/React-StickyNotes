42fba9f31aded2fd6e6a89c0dc41397fa7ee37c7
SP <74065235+SPchalil@users.noreply.github.com>
2021-02-05 00:14:33 -0330

    position rendering

--- src/Board.jsx
+++ src/Board.jsx
@@ -6,19 +6,36 @@ class Board extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-          history: [{stickers: Array(10).fill(null)}]
+            currentStickyNotes: null
+            
         };
       } 
 
-    handleClick() {
-        alert("Hello! I am an alert box!!");
-        //<StickyNote text = "Work" positionX= "100px" positionY = "150px" color = "black" bgColor = "blue"/> ;
+    
+    renderStickyNotes(){
+        const currentStickyNotes = this.state.currentStickyNotes;
+        const Xmax = 500;
+        const Ymax = 50;
+        const Xmin = 0;
+        const Ymin = 0;
+        
+        let posX = getRandomXInt(Xmin, Xmax);
+        let posY = getRandomYInt(Ymin, Ymax);
+        console.log (posX);
+        console.log (posY);
+        
+        
+        this.setState({currentStickyNotes: <StickyNote text = "Work" positionX= {posX} positionY = {posY} color = "black" bgColor = "pink"/>})
+        
+
     }
 
+    
     render(){
         return(
             <div className="App">
-                <header className="App-header" onClick={this.handleClick}>
+                <header className="App-header" onClick={() => this.renderStickyNotes()}> 
+                    {this.state.currentStickyNotes}
                     <img src={postit} className="App-logo" alt="postit" />
                     <div className = "heading"> <h1>Sticky Notes</h1> </div>
                     <div className = "ref"> <h2 className = "name"> .. by Swapna <a className = "link" href="https://github.com/SPchalil/React-StickyNotes"> github </a>  </h2> </div>
@@ -34,5 +51,21 @@ class Board extends React.Component{
     }
     
 };
+
+function getRandomXInt(Xmin, Xmax) {
+    const min = Math.ceil(Xmin);
+    const max = Math.floor(Xmax);
+    const num =  Math.floor(Math.random() * (max - min + 1) + min);
+    return num.toString()+"px"; 
+  }
+  
+function getRandomYInt(Ymin, Ymax) {
+    const min = Math.ceil(Ymin);
+    const max = Math.floor(Ymax);
+    const num =  Math.floor(Math.random() * (max - min + 1) + min);
+    return num.toString()+"px"; 
+  }
+
+
 
 export default Board;
