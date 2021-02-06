053b1ab931bcc5505fcf9aa4159fa94900791118
SP <74065235+SPchalil@users.noreply.github.com>
2021-02-06 09:56:43 -0330

    Sticky note generated onclick

--- src/Board.jsx
+++ src/Board.jsx
@@ -6,23 +6,63 @@ class Board extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            currentStickyNotes: []
-            
+            currentStickyNotes: [],
+            height: 0,
+            width:0,
+            //x: 0, y: 0 
         };
+        this.resizeHandler = this.resizeHandler.bind(this);
+        
       } 
-
+      
     
-    addStickyNotes(){
+    resizeHandler() {
+        const width = this.divElement.clientWidth;
+        const height = this.divElement.clientHeight;
+        this.setState({ width, height });
+    }
+    componentDidMount() {
+        this.resizeHandler();
+        window.addEventListener('resize', this.resizeHandler);
+      }
+    
+      componentWillUnmount(){
+        window.removeEventListener('resize', this.resizeHandler);
+      }
+/*
+    componentDidMount() {
+        const height = this.divElement.clientHeight;
+        this.setState({ height });
+        const width = this.divElement.clientWidth;
+        this.setState({ width });
+    }
+    */
+    addStickyNotes(e){
         const currentStickyNotes = this.state.currentStickyNotes;
-        const Xmax = 1160;
-        const Ymax = 560;
+        const Xmax = this.state.width-200;
+        const Ymax = this.state.height-200;
         const Xmin = 0;
         const Ymin = 0;
         
-        let posX = getRandomXInt(Xmin, Xmax);
-        let posY = getRandomYInt(Ymin, Ymax);
-        console.log (posX);
-        console.log (posY);
+        //let posX = getRandomXInt(Xmin, Xmax);
+        //let posY = getRandomYInt(Ymin, Ymax);
+        //console.log (posX);
+        //console.log (posY);
+
+        let posX = e.nativeEvent.offsetX;
+        let posY = e.nativeEvent.offsetY;
+        //console.log (posX);
+        //console.log (posY);
+        if (posX > Xmax) {
+            posX = e.nativeEvent.offsetX-200;
+        }
+        
+
+        if (posY > Ymax) {
+            posY = e.nativeEvent.offsetY-200;
+        }
+        
+        
 
         
         let bcolor = generateColor ();
@@ -51,8 +91,9 @@ class Board extends React.Component{
         
         return(
             <div className="App">
-                <header className="App-header" onClick={() => this.addStickyNotes()}> 
+                <header className="App-header" onClick={(e) => this.addStickyNotes(e)} ref={ (divElement) => { this.divElement = divElement } }> 
                     {this.renderStickyNotes()}
+                    
                     <img src={postit} className="App-logo" alt="postit" />
                     <div className = "heading"> <h1>Sticky Notes</h1> </div>
                     <div className = "ref"> <h2 className = "name"> .. by Swapna <a className = "link" href="https://github.com/SPchalil/React-StickyNotes"> github </a>  </h2> </div>
@@ -68,6 +109,7 @@ class Board extends React.Component{
     }
     
 };
+//Size: width: <b>{this.state.width}px</b>, height: <b>{this.state.height}px</b>
 
 function getRandomXInt(Xmin, Xmax) {
     const min = Math.ceil(Xmin);
