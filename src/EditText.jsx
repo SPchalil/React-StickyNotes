1664614b842249873f31cbcdd88c0959ea2a58bf
SP <74065235+SPchalil@users.noreply.github.com>
2021-02-16 23:56:26 -0330

    -

--- src/EditText.jsx
+++ src/EditText.jsx
@@ -47,56 +47,6 @@ const styles = StyleSheet.create({
 
 */
 /*-----------------------------------------------------------------------------------*/
-/*
-import React from 'react';
-import { View, StyleSheet,TextInput} from 'react-native';
-//import React, { useState } from 'react';
-
-export default function EditText(props){
-
-
-
-  return(
-
-    <View style={styles.container}>
-      
-      <TextInput
-      multiline
-      numberOfLines = {40}
-      keyboardType= 'default'
-      style={styles.input} 
-      placeholder = 'Take a note..' 
-      onChangeText={(props.onChange)}
-      defaultValue = {props.text}
-      />
-    </View>
-
-  );
-  
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-
-  input:{
-    borderWidth:2,
-    borderColor:'#C7CFB7',
-    padding: 5,
-    margin:5,
-    width:150,
-    height: 150
-
-  }
-
-});
-*/
-
-
 
 import React from 'react';
 import { View, StyleSheet,TextInput} from 'react-native';
@@ -108,14 +58,11 @@ constructor(props){
  text: props.text,
  onChange: props.onChange
   }
-  
 }
-
 
 render(){
   return(
     <View style={styles.container}>
-      
       <TextInput
       multiline
       numberOfLines = {40}
@@ -124,12 +71,12 @@ render(){
       placeholder = 'Take a note..' 
       onChangeText={this.state.onChange}
       defaultValue = {this.state.text}
-      
       />
     </View>
   );
 }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -144,9 +91,7 @@ const styles = StyleSheet.create({
     margin:5,
     width:150,
     height: 150
-
   }
-
 });
 
 export default EditText;
