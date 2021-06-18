0128b1eec10d33adcc38112077ed2043ac58d9ba
Swapna Chalil <74065235+SPchalil@users.noreply.github.com>
2021-06-18 00:01:37 -0230

    -

--- src/Register.js
+++ src/Register.js
@@ -78,7 +78,7 @@ class Register extends React.Component {
         let email = this.state.email;
         let password = this.state.password;
         if (!errors) {
-
+            // merging two api calls
            /* api.post(`/registration`,
                     {username:`${username}`,
                     email:`${email}`, 
@@ -109,7 +109,7 @@ class Register extends React.Component {
             .then(res => {
                 console.log(res.data);
                 //console.log("No Records");
-                if (res.data.length === 0){    //res.data.length
+                if (res.data.length === 0){    
                     console.log("No Records");
                     api.post(`/register`,
                     {username:`${username}`,
@@ -131,14 +131,9 @@ class Register extends React.Component {
                     alert("User already exists!Please log in");
                     this.props.history.push("/login"); 
                 }
-                //this.props.history.push("/registered");
                 
                   });
                   
-        
-       
-        //this.props.history.push("/registered");     
-
         } 
         else {
             alert(errors);
@@ -230,22 +225,3 @@ class Register extends React.Component {
 }
 export default withRouter(Register);
 
-/*-------------------Backend- Post a new user---------------------
-
-        //const userid = 1;
-        //let username = this.state.username;
-        //let email = this.state.email;
-        //let password = this.state.password;
-       
-        api.post(`/users`,
-         {username:`${username}`,
-         email:`${email}`, 
-         password:`${password}`
-         
-        })  
-        .then(res => {  
-        console.log(res);  
-        console.log(res.data); 
-        }); 
-    
-    */
